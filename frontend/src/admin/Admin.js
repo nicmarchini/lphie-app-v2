3a090ad6ec0bb35117f6dfa2f47a4d04cc487f66
Nic <nicmarchini@gmail.com>
2022-07-17 14:48:18 -0500

    basic plan for admin page, draft

---
+++ frontend/src/admin/Admin.js
@@ -0,0 +1,10 @@
+function Admin() {
+    return (
+      <div>
+        test
+      </div>
+    );
+  }
+  
+export default Admin;
+  
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+// import App from './App';
+import Admin from './admin/Admin';
 // import reportWebVitals from './reportWebVitals';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Admin />
   </React.StrictMode>
 );
 
